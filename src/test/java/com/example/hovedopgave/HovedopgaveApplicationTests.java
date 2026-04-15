ff313bcd8ae60d7411438bccfc664e1369b7c375
nunu <nuso0001@stud.ek.dk>
2026-04-15 10:38:19 +0200

    første commit

---
+++ src/test/java/com/example/hovedopgave/HovedopgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hovedopgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HovedopgaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
